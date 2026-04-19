92ba74edf9936bd7dd2961d72d5d4992aa355143
InternetBowser <internetbowsergd@gmail.com>
2026-04-19 15:23:41 -0500

    Bring topbar to seperate file

--- apps/web/app/(with-topbar)/layout.tsx
+++ apps/web/app/(with-topbar)/layout.tsx
@@ -1,46 +1,13 @@
-"use client"
-
-import {
-  ClerkLoading,
-  Show,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs"
-import TextLogo from "@workspace/ui/components/branding/text-logo"
-import { Button } from "@workspace/ui/components/button"
-import Link from "next/link"
-import { useRouter } from "next/navigation"
+import Topbar from "@/components/ui/topbar"
 
 export default function TopbarLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const router = useRouter()
-
   return (
     <div className="h-screen">
-      <header className="z-20 flex h-16 w-full items-center justify-between gap-4 border-b border-dashed bg-accent/10 p-4">
-        <Link href="/" className="flex gap-4">
-          <TextLogo />
-        </Link>
-        <div className="flex items-center gap-4">
-          <Show when="signed-out">
-            <SignInButton />
-            <SignUpButton>
-              <Button variant="default">Sign Up</Button>
-            </SignUpButton>
-          </Show>
-          <Show when="signed-in">
-            <Button onClick={() => router.push("/dashboard")}>Dashboard</Button>
-            <div className="aspect-square! h-[28px]">
-              <UserButton />
-            </div>
-          </Show>
-          <ClerkLoading>Loading User...</ClerkLoading>
-        </div>
-      </header>
+      <Topbar />
       <div className="flex min-h-[calc(100svh-64px)] flex-col">{children}</div>
     </div>
   )
---
+++ apps/web/components/ui/topbar.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import {
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  ClerkLoading,
+} from "@clerk/nextjs"
+import TextLogo from "@workspace/ui/components/branding/text-logo"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { Button } from "@workspace/ui/components/button"
+
+export default function Topbar() {
+  const router = useRouter()
+  return (
+    <>
+      <header className="z-20 flex h-16 w-full items-center justify-between gap-4 border-b border-dashed bg-accent/10 p-4">
+        <div>
+          <Link href="/" className="flex gap-4">
+            <TextLogo />
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Show when="signed-out">
+            <SignInButton />
+            <SignUpButton>
+              <Button variant="default">Sign Up</Button>
+            </SignUpButton>
+          </Show>
+          <Show when="signed-in">
+            <Button onClick={() => router.push("/dashboard")}>Dashboard</Button>
+            <div className="aspect-square! h-[28px]">
+              <UserButton />
+            </div>
+          </Show>
+          <ClerkLoading>Loading User...</ClerkLoading>
+        </div>
+      </header>
+    </>
+  )
+}
